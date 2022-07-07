cde3a921c8ed4b0d162ff222792f3bc14f7b32ee
ankush <ankushkr2898@gmail.com>
2022-07-07 18:15:33 +0530

    add.py added

---
+++ add.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  7 18:09:06 2022
+
+@author: ankus
+"""
+x = int(input('Enter first number: '))
+y = int(input('Enter second number: '))
+
+print("Sum is: ",x+y)
