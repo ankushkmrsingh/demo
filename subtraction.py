4eeeb1beedd402aabc952454fb65219d2e3e7d20
ankush <ankushkr2898@gmail.com>
2022-07-07 18:26:06 +0530

    added subtraction

---
+++ subtraction.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  7 18:23:31 2022
+
+@author: ankus
+"""
+
+x = int(input('Enter first number: '))
+y = int(input('Enter second number: '))
+
+print("Subtraction is: ",x+y)
